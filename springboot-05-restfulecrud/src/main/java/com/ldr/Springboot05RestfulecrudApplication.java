cf16aaf2b81734ceb55becda2e6e6d2348c557a7
t4ki598 <t4ki598@163.com>
2020-04-07 00:50:02 +0800

    静态资源映射相关操作和thymeleaf语法学习

---
+++ springboot-05-restfulecrud/src/main/java/com/ldr/Springboot05RestfulecrudApplication.java
@@ -0,0 +1,13 @@
+package com.ldr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot05RestfulecrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot05RestfulecrudApplication.class, args);
+    }
+
+}
--- springboot-05-restfulecrud/src/main/java/com/ldr/controller/TestController.java
+++ springboot-05-restfulecrud/src/main/java/com/ldr/controller/TestController.java
@@ -1,9 +1,25 @@
 package com.ldr.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.Map;
+
 /**
  * 类 描 述：TODO
  * 创建时间：2020-04-06 16:19
  * 创 建 人：t4ki
  */
+
+@Controller
 public class TestController {
+
+    //查出一些数据在页面中显示
+    @RequestMapping("/test")
+    public String test(Map<String, Object> map){
+        map.put("hello", "<h2>你好</h2>");
+        map.put("users", Arrays.asList("张三", "李四", "王五"));
+        return "test";
+    }
 }
