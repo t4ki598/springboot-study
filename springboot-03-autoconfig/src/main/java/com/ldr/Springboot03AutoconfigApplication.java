9f80d5fbf6e0888a1d59624c5e0c6ff6b78cccb7
t4ki598 <t4ki598@163.com>
2020-04-08 15:35:31 +0800

    SpringBoot自动装配原理剖析（略过）

--- springboot-03-autoconfig/src/main/java/com/ldr/Springboot03AutoconfigApplication.java
+++ springboot-03-autoconfig/src/main/java/com/ldr/Springboot03AutoconfigApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springboot03AutoconfigApplication {
 
     public static void main(String[] args) {
+        System.out.println("自动配置原理");
         SpringApplication.run(Springboot03AutoconfigApplication.class, args);
     }
 
